50eb85c6a8cd4366aca166b9cf1939b991d170fa
DeveloperKairo <contatodevkairo@gmail.com>
2025-11-04 10:52:06 -0300

    feat: add draw cards logic

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -11,7 +11,7 @@ const state = {
   },
   fieldCards:{
     player: document.getElementById("player-field-card"),
-    computer: document.getElemenyById("computer-field-card"),
+    computer: document.getElementById("computer-field-card"),
   },
   actions: {
     button: document.getElementById("next-duel"),
@@ -19,11 +19,11 @@ const state = {
 };
 
 const playerSides = {
-  player: "player-field-card",
-  computer: "computer-field-card"
+  player: "player-cards",
+  computer: "computer-cards"
 }
 
-const pathImages = ".src/assets/icons/";
+const pathImages = "./src/assets/icons/";
 
 const cardData = [
   {
@@ -51,6 +51,37 @@ const cardData = [
     LoseOf:[2],
   }
 ]
+
+async function getRandomCardId() {
+  const randomIndex = Math.floor(Math.random() * cardData.length);
+  return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide){
+  const cardImage = document.createElement("img");
+  cardImage.setAttribute("height", "100px");
+  cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+  cardImage.setAttribute("data-id", IdCard);
+  cardImage.classList.add("card");
+
+  if(fieldSide === playerSides.player){
+    cardImage.addEventListener("click", () => {
+      setCardsField(cardImage.getAttribute("data-id"));
+    });
+
+    cardImage.addEventListener("mouseover", () => {
+    drawSelectCard(IdCard);
+    });
+  }
+
+  return cardImage;
+}
+
+async function drawSelectCard(index){
+  state.cardSprites.avatar.src = cardData[index].img;
+  state.cardSprites.name.innerText = cardData[index].name;
+  state.cardSprites.type.innerText = "Attribute : " + cardData[index].type;
+}
 
 async function drawCards(cardNumbers, fieldSide) {
   for(let i = 0; i < cardNumbers; i++){
